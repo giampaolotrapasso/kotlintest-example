f5521812fa566e2d2f848f8d2c2d317d893a6697
Giampaolo Trapasso <giampaolo.trapasso@gmail.com>
2019-05-07 10:45:49 +0200

    fixed import

--- src/test/kotlin/coffee/MyTest.kt
+++ src/test/kotlin/coffee/MyTest.kt
@@ -1,12 +1,12 @@
 package coffee
 
-import io.kotlintest.matchers.haveLength
+import io.kotlintest.matchers.startWith
 import io.kotlintest.should
+import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 
 class MyTests : StringSpec({
   "length should return size of string" {
-     assert(false)
     "hello".length shouldBe 5
   }
   "startsWith should test for a prefix" {
